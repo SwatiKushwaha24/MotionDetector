9b1dd16ede7ad8b6b8bcdfde8170e1281c4d7f50
Swati Kushwaha <swatikushwaha24032003@gmail.com>
2022-10-03 00:55:09 +0530

    Add files via upload
    This file contains the code to plot the graph from previously created csv file and for that I imported the other file containing motion detection code into this one.

---
+++ plotting.py
@@ -0,0 +1,28 @@
+from motion_detector import df
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+from bokeh.models import HoverTool, ColumnDataSource
+
+# df["Start_string"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+# df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+cds = ColumnDataSource(df)
+p = figure(x_axis_type = "datetime", width = 500, height = 250, sizing_mode = "stretch_both")
+p.title.text = "Motion Graph"
+p.title.text_color = "black"
+p.title.text_font = "arial"
+p.yaxis.minor_tick_line_color = None
+p.yaxis[0].ticker.desired_num_ticks = 1
+hover = HoverTool(
+    tooltips = [
+        ("Start", "@Start{%Y-%m-%d %H:%M:%S}"),
+        ("End", "@End{%Y-%m-%d %H:%M:%S}")
+    ],
+    formatters = {
+        "@Start" : "datetime",
+        "@End" : "datetime"
+    }
+)
+p.add_tools(hover)
+q = p.quad(left = "Start", right = "End", top = 0, bottom = 1, color = "green", source =cds)
+output_file("Graph.html")
+show(p)
